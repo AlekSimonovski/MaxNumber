58561675eee8f461d19a29a79743368d8e4a36ba
Александар Симоновски <alek.simonovski02@gmail.com>
2023-04-14 15:57:28 +0200

    Moving files from branch development to master branch.

--- src/MaxNumber.java
+++ src/MaxNumber.java
@@ -1,4 +1,21 @@
-
+import java.util.Scanner;
 public class MaxNumber {
-
+		public static void main(String[] args) {
+			Scanner input = new Scanner(System.in);
+			System.out.println("Largest number from N loaded numbers: ");
+			int n, x, max;
+			System.out.println("Enter the amount of numbers you want to check: ");
+			n = input.nextInt();
+			System.out.println("Enter one number: ");
+			x = input.nextInt();
+			max = x;
+			for (int i=2; i<=n; i++) {
+				System.out.println("Enter "+ i +". number: ");
+				x = input.nextInt();
+				if (x>max) 
+				max = x;
+			}
+			System.out.println("The max number out of the "+ i +" entered numbers is: " +max);
+			
+		}
 }
